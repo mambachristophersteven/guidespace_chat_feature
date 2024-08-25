1f6e7d87d474c76d2876e459581360303e5645fe
Christopher Steven <christopherstevennunoo@gmail.com>
2024-08-25 18:02:40 +0000

    login page ui finished

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <div class="container">
+        <div class="h2">Login</div>
+        <form action="">
+            <input type="text" name="username" id="username" placeholder="username">
+            <input type="password" name="password" id="password" placeholder="password">
+            <input type="submit" value="login">
+        </form>
+    </div>
+</body>
+</html>
